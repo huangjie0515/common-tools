b8e190f61330fcd38805e71542389a69246308f0
huangjie0515 <33750686+huangjie0515@users.noreply.github.com>
2020-05-11 22:26:18 +0800

    第一次

---
+++ src/main/java/io/github/huangjie0515/commontools/CommonToolsApplication.java
@@ -0,0 +1,13 @@
+package io.github.huangjie0515.commontools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/huangjie0515/commontools/CommonToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.huangjie0515.commontools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
